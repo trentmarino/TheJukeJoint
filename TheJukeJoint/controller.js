6896f97a3b2066b68f686918ff6ca61cca7f2afc
trentmarino <trentmario@hotmail.com>
2016-05-16 16:37:50 +1000

    made it ready for styling

--- TheJukeJoint/controller.js
+++ TheJukeJoint/controller.js
@@ -4,7 +4,7 @@
 
 
 (function (){
-    var recordCanvas = document.getElementById('bgCanvas');
+    var recordCanvas = document.getElementById('recordCanvas');
     var ncanvas = document.getElementById("needle");
     ncanvas.width = 500;
     ncanvas.height = 500;
@@ -78,9 +78,13 @@
                 ctxRecord.save();
 
                 if (speed ===  $('#volume_speed').val()) {
-                    ctxRecord.translate(cache.width / 1.225, cache.height / 0.9); //let's translate
+                    ctxRecord.clearRect(0, 0, canvas.width, canvas.height*2); //clear the canvas
+                    ctxRecord.translate(cache.width/1.8, cache.height/1.75); //let's translate
                     ctxRecord.rotate(Math.PI / 180 * (ang += 4) * ($('#volume_speed').val())); //increment the angle and rotate the image
-                    ctxRecord.drawImage(img, -cache.width / 1.535, -cache.height / 1.535, cache.width * 1.3, cache.height * 1.3); //draw the image ;)
+                    ctxRecord.drawImage(img, -cache.width / 1.8, -cache.height / 1.8, cache.width*1.1, cache.height*1.1); //draw the image ;)
+                    //  ctxRecord.translate(cache.width / 1.8, cache.height / 1.35); //let's translate
+                    // ctxRecord.rotate(Math.PI / 180 * (ang += 4) * ($('#volume_speed').val())); //increment the angle and rotate the image
+                    // ctxRecord.drawImage(img, -cache.width / 1.3, -cache.height / 0.95, cache.width * 1.05, cache.height * 1.05); //draw the image ;)
                     console.log(time);
                     console.log(currentTime.getCurrentTime());
                     stopPlaying = time - currentTime.getCurrentTime();
@@ -89,14 +93,14 @@
                         speed = 0;
                     }
                 } else if (speed === 0) {
-                    ctxRecord.translate(cache.width / 1.225, cache.height / 0.9); //let's translate
+                    ctxRecord.translate(cache.width/1.8, cache.height/1.75); //let's translate
                     ctxRecord.rotate(Math.PI / 180 * (ang += 0)); //increment the angle and rotate the image
-                    ctxRecord.drawImage(img, -cache.width / 1.535, -cache.height / 1.535, cache.width * 1.3, cache.height * 1.3); //draw the image ;)
+                    ctxRecord.drawImage(img, -cache.width / 1.8, -cache.height / 1.8, cache.width*1.1, cache.height*1.1); //draw the image ;)
                 }
 
                 else if (speed === "load") {
-                    ctxRecord.translate(cache.width / 1.225, cache.height / 0.9); //let's translate
-                    ctxRecord.drawImage(img, -cache.width / 1.535, -cache.height / 1.535, cache.width * 1.3, cache.height * 1.3); //draw the image ;)
+                    ctxRecord.translate(cache.width/1.8, cache.height/1.75); //let's translate
+                    ctxRecord.drawImage(img, -cache.width / 1.8, -cache.height / 1.8, cache.width*1.1, cache.height*1.1); //draw the image ;)
                 }
                 else if (speed === "unload") {
                     ctxRecord.clearRect(0, 0, recordCanvas.width, recordCanvas.height);
@@ -112,13 +116,13 @@
     }
 
 
-    function drawPlayer() {
-
-        imageObj.onload = function () {
-            ctxTurntable.drawImage(imageObj, turntableCanvas.width / 15, turntableCanvas.height / 4.3, 900 / 1.75, 660 / 2);
-        };
-        imageObj.src = 'recordPlayer.png';
-    }
+    // function drawPlayer() {
+    //
+    //     imageObj.onload = function () {
+    //         ctxTurntable.drawImage(imageObj, turntableCanvas.width / 15, turntableCanvas.height / 4.3, 900 / 1.75, 660 / 2);
+    //     };
+    //     imageObj.src = 'recordPlayer.png';
+    // }
     spiningRecord();
 
     drawPlayer();
--- TheJukeJoint/index.php
+++ TheJukeJoint/index.php
@@ -7,6 +7,7 @@
     <title>Title</title>
     <meta charset="UTF-8">
     <link rel="stylesheet" type="text/css" href="mainstyle.css">
+    <link rel="stylesheet" type="text/css" href="grid.css">
     <script></script>
     <script src="https://code.createjs.com/easeljs-0.8.2.min.js"></script>
     <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
@@ -37,29 +38,23 @@ include "equalizer.php";
 
 </script>
 
+<div class="row container">
 
-<div id="bgDiv">
-    <div id="leftBackgroundImage">
-        <img src="leftBG2.png" height="100%" width="100%">
-    </div>
     <div id="visualizer_wrapper">
-        <canvas id='canvas' width="800" height="350" ></canvas>
+        <canvas id='canvas' width="800" height="350"></canvas>
     </div>
- 
-    <canvas id="needle"></canvas>
-    <canvas id="turntable" height=" 500" width=" 600"></canvas>
-
-    <canvas id="bgCanvas"></canvas>
-
-    <div id="trackList">
+    <div id="trackList" class="col trackList">
         <div id="infoPanel">
 
         </div>
     </div>
-    <div class="yt" id="playerFrame">
-
+   
+    <div class="col player">
+        <canvas id="needle"></canvas>
+        <canvas id="turntable" height=" 500" width=" 600"></canvas>
+        <div id="player" class="playing"></div>
+        <canvas id="recordCanvas" class="recordCanvas"></canvas>
     </div>
-    <div id="player" class="playing"></div>
     <script src="trackLoader.js"></script>
     <script>
         $('#insert_songs').submit(function () {
@@ -69,25 +64,30 @@ include "equalizer.php";
             return true;
         });
     </script>
-
     <script src="controller.js"></script>
-    <menu id="controls">
-        <button id="play">Play</button>
-        <button id ="pause">Pause</button>
-        <button id="eject">Eject</button>      
-        <button id = "resume">Resume</button>
-        <div>
-        <input id = "volume_range" type="range" name="volume"  min="0" max="2" step="0.1" value="1" >
-        <input id = "volume_speed" type="range" name="volume"  min="0" max="2" step="0.5" value="1" >
+    <div class="col ytPlayer">
+        <div class="yt" id="playerFrame">
         </div>
-    </menu>
-    <script src="youtubePlayer.js"></script>
-
-
+    </div>
+    <div class="col controls">
+        <button id="play">Play</button>
+        <button id="pause">Pause</button>
+        <button id="eject">Eject</button>
+        <button id="resume">Resume</button>
+        <div>
+            <input id="volume_range" type="range" name="volume" min="0" max="2" step="0.1" value="1">
+            <input id="volume_speed" type="range" name="volume" min="0" max="2" step="0.5" value="1"  list="steplist">
+            <datalist id="steplist">
+                <option>0</option>
+                <option>0.5</option>
+                <option>1</option>
+                <option>1.5</option>
+                <option>2</option>
+            </datalist>
+        </div>
+    </div>
 </div>
 
-
-
-
+    <script src="youtubePlayer.js"></script>
 </body>
 </html>
